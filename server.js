188570ed7c0662b09d85a96cdf7be28fe1003047
Timea <timea.reich@gmail.com>
2022-06-25 14:26:06 +0100

    fixing deploy error

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.delete("/customers/:customerId", (req, res) => {
       }
     });
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT;
 console.log(port);
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
